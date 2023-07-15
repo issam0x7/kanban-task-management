9c8f74d9cce40e3f7d484cd173f7b8ae89d1571e
issam-riyahi <issamriyahi2@gmail.com>
2023-07-16 00:00:12 +0100

    add all template css variable

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,28 @@
+import { Input } from "@/components/ui/Input";
+import Button from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+      <Button variant="destructive" />
+      <Input type="text" placeholder="text"></Input>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
     </main>
   )
 }
