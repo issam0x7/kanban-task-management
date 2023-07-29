a60b332cf55ec4129dbc12f81842ff516a1a784b
Mohamed <mdahni06@gmail.com>
2023-07-29 23:47:23 +0100

    refactored app.js

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
 
 const boardRouters = require('./routes/boardRoutes');
 
 var app = express();
 
-// middlewares
-app.use(logger('dev'));
+app.use(logger());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// routes
 app.use('/api/boards', boardRouters);
+
+app.use(errorHandler);
 
 module.exports = app;
