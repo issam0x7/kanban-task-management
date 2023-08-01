421307e1d6726b5edb9fa85620b428e6849ae15c
issam-riyahi <issamriyahi2@gmail.com>
2023-08-01 23:27:42 +0100

    add lucide icon

--- frontend/src/components/taskBoardToggler.tsx
+++ frontend/src/components/taskBoardToggler.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
 import Link from "next/link"
-
+import { LayoutDashboard  } from "lucide-react";
 
 
 const TaskBoardToggler = () => {
   return (
-    <div className="task-toggler py-3 hover:bg-primary hover:text-white ps-6 w-[85%] rounded-e-full">
-      <Link href="#" className="flex items-center gap-4" >
-        <Image src="/board.svg" alt="board-icon" width={16} height={16} className="fill-lines" />
+    <div className="task-toggler py-3 hover:bg-primary hover:text-white ps-6 w-[85%] rounded-e-full mt-4">
+      <Link href="#" className="flex items-center gap-4 font-bold" >
+        <LayoutDashboard />
         test
       </Link>
     </div>
